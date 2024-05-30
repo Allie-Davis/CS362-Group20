9df829d1b4ee06d1f975ec98a1f68b0521aef22d
Muhammad Akbar <muhammadakbarwork@gmail.com>
2024-05-29 21:36:44 -0700

    Added function 2 tests and deleted dummy test

--- tests.py
+++ tests.py
@@ -1,10 +1,15 @@
 import unittest
+from task import my_datetime
 
 
-class TestCase(unittest.TestCase):
+class TestMyDatetime(unittest.TestCase):
+    def test_epoch(self):
+        self.assertEqual(my_datetime(0), '01-01-1970')
 
-    def test1(self):
-        self.assertTrue(True)
+    def test_given_examples(self):
+        self.assertEqual(my_datetime(123456789), '11-29-1973')
+        self.assertEqual(my_datetime(9876543210), '12-22-2282')
+        self.assertEqual(my_datetime(201653971200), '02-29-8360')
 
 
 if __name__ == '__main__':
